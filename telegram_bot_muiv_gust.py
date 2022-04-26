1f5187aaf2163496eb2ca1053a102930a6fdce5b
TheGustOff <apsrandr@mail.ru>
2022-04-26 05:13:21 +0300

    info echo bot wiki bot

--- telegram_bot_muiv_gust.py
+++ telegram_bot_muiv_gust.py
@@ -1,7 +1,102 @@
+import types
+
 import telebot
+import telebot, wikipedia, re
 from config import *
+from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
+
+'''@bot.message_handler(commands=['get_info', 'info'])
+def get_user_info(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    item_yes = types.InlineKeyboardButton(text='да', callback_data='yes')
+    item_no = types.InlineKeyboardButton(text='нет', callback_data='no')
+
+    markup_inline.add(item_yes, item_no)
+    bot.send_message(message.chat.id, 'Желаете узнать информацию о Вас',
+        reply_markup=markup_inline
+    )
+
+@bot.callback_query_handler(func=lambda call:True)
+def answer(call):
+    if call.data == 'yes':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item_id = types.KeyboardButton('МОЙ ID')
+        item_username = types.KeyboardButton('МОЙ НИК')
+
+        markup_reply.add(item_id, item_username)
+        bot.send_message(call.message.chat.id, 'Нажмите на одну из кнопок',
+            reply_markup=markup_reply
+        )
+    elif call.data == 'no':
+        pass
+
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+    if message.text == 'МОЙ ID':
+        bot.send_message(message.chat.id, f'Your ID: {message.from_user.id}')
+    elif message.text == 'МОЙ НИК':
+        bot.send_message(message.chat.id, f'Your ID: {message.from_user.first_name} {message.from_user.last_name}')'''
+
+
+# ЭХО-БОТ
+'''@bot.message_handler(commands=['start'])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)'''
+
+
+# WIKIPEDIA-БОТ
+wikipedia.set_lang("ru")
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext=ny.content[:1000]
+        wikimas=wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        return wikitext2
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
+
+
+////
+
+
+'''@bot.message_handler(commands=['stop'])
+def stop(message):
+  if message.from_user.username == cfg.Father:
+    pid = str(os.getpid())
+    stoper = open('ozerx/stoper.bat', 'w')
+    stoper.write("Taskkill /PID " + pid + " /F")
+    stoper.close()
+    os.system('C:/Users/smp/Desktop/SMP/ozerx/stoper.bat')
+  else:
+    bot.send_message(message.chat.id, "Ты не Создатель бота; у тебя нет админ-прав, проваливай!")'''
+
 
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
@@ -14,7 +109,15 @@ def welcome_start(message):
 def welcome_help(message):
     print(message.from_user)
     bot.send_message(408505187, 'Hello')
-    bot.send_message(message.chat.id, 'Или мне показалось. Надеюсь, что ты мне написал /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Надеюсь, что вы мне написали /start', reply_markup=keyboard1)
+
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard1.row('Привет', 'Пока')
+keyboard1.row('/start', '/stop')
+keyboard1.row('За', 'Против')
+keyboard1.row('Слава', 'России')
+keyboard1.row('info')
 
 
 '''@bot.message_handler(content_types=['sticker'])
@@ -38,11 +141,7 @@ def send_text(message):
         bot.send_message(message.chat.id, 'И тебе здарова')
 
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Привет', 'Пока')
-keyboard1.row('/start', '/help')
-keyboard1.row('За', 'Против')
-keyboard1.row('Слава', 'России')
+'''reply_markup=keyboard1'''
 
 
 @bot.message_handler(content_types=['audio'])
@@ -68,6 +167,7 @@ def fifth(message):
 @bot.message_handler(content_types=['document', 'audio', 'video', 'photo'])
 def sixth(message):
     bot.send_message(message.chat.id, 'Всё сразу')
+
 
 
 '''@bot.message_handler(content_types=['text'])
@@ -124,4 +224,6 @@ keyboard1.row('/start', '/help')
 keyboard1.row('Слава', 'России')'''
 
 
-bot.polling()
+'''bot.polling()'''
+bot.polling(none_stop=True, interval=0)
+
