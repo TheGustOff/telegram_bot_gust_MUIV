2683425c0032801194b9df6408230724eceb8b3e
509 <a@a.b>
2022-04-27 15:12:12 +0300

    echo bot fix

---
+++ echo_bot.py
@@ -0,0 +1,32 @@
+from telebot import types
+import telebot, wikipedia, re
+from config import *
+
+bot = telebot.TeleBot(TOKEN)
+
+# ЭХО-БОТ
+IDLE = 0
+LISTENING_TO_COMMANDS = 1
+bot_state = IDLE
+
+@bot.message_handler(commands=['start'])
+def start(m, res=False):
+    print("qq")
+    global bot_state
+    bot_state = LISTENING_TO_COMMANDS
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+@bot.message_handler(commands=['stop'])
+def stop(m, res=False):
+    global bot_state
+    bot_state = IDLE
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if bot_state != IDLE:
+        bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+
+
+bot.polling(none_stop=True, interval=0)
+
--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-import types
-
-import telebot
-import telebot, wikipedia, re
-from config import *
-from telebot import types
-
-bot = telebot.TeleBot(TOKEN)
-
-'''@bot.message_handler(commands=['get_info', 'info'])
+@bot.message_handler(commands=['get_info', 'info'])
 def get_user_info(message):
-    markup_inline = types.InlineKeyboardMarkup()
+    markup_inline = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     item_yes = types.InlineKeyboardButton(text='да', callback_data='yes')
     item_no = types.InlineKeyboardButton(text='нет', callback_data='no')
 
     markup_inline.add(item_yes, item_no)
     bot.send_message(message.chat.id, 'Желаете узнать информацию о Вас',
-        reply_markup=markup_inline
-    )
+        reply_markup=markup_inline)
+
 
 @bot.callback_query_handler(func=lambda call:True)
 def answer(call):
     if call.data == 'yes':
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         item_id = types.KeyboardButton('МОЙ ID')
         item_username = types.KeyboardButton('МОЙ НИК')
 
         markup_reply.add(item_id, item_username)
         bot.send_message(call.message.chat.id, 'Нажмите на одну из кнопок',
-            reply_markup=markup_reply
-        )
+            reply_markup=markup_reply)
     elif call.data == 'no':
         pass
 
@@ -37,18 +27,7 @@ def get_text(message):
     if message.text == 'МОЙ ID':
         bot.send_message(message.chat.id, f'Your ID: {message.from_user.id}')
     elif message.text == 'МОЙ НИК':
-        bot.send_message(message.chat.id, f'Your ID: {message.from_user.first_name} {message.from_user.last_name}')'''
-
-
-# ЭХО-БОТ
-'''@bot.message_handler(commands=['start'])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
-
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)'''
-
+        bot.send_message(message.chat.id, f'Your ID: {message.from_user.first_name} {message.from_user.last_name}')
 
 # WIKIPEDIA-БОТ
 wikipedia.set_lang("ru")
@@ -81,9 +60,6 @@ def handle_text(message):
     bot.send_message(message.chat.id, getwiki(message.text))
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
-
-
-////
 
 
 '''@bot.message_handler(commands=['stop'])
@@ -225,5 +201,3 @@ keyboard1.row('Слава', 'России')'''
 
 
 '''bot.polling()'''
-bot.polling(none_stop=True, interval=0)
-
